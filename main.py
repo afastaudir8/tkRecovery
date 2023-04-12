ca5767a684aec22edf5173239085ba90f7620bb8
0x3ff <benbornapersonal@gmail.com>
2023-04-12 00:32:44 -0400

    basic window done

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import tkinter
 import subprocess
+
+from tkinter import *
+
+rootWin = Tk()
+
+rootWin.title('tkRecovery (0x3ff, a fast audi r8)')
+rootWin.geometry('500x500')
+
+
+
+rootWin.mainloop()
