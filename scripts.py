7b92ce5ac03364f40a715b1f87d40881365eeb2c
afastaudir8 <ervd516@gmail.com>
2023-04-14 00:19:43 -0400

    status now works (only on Linux right now)

--- scripts.py
+++ scripts.py
@@ -2,15 +2,18 @@ import subprocess
 import platform
 
 binos = platform.system()
-devicestatus = "No device found!"
+
 
 def deviceconnected():
+    global devicestatus
     if binos == 'Linux':
         deviceid = subprocess.check_output(["lsusb"])
+        deviceid = str(deviceid)
         if "12a8" in deviceid:
             devicestatus = "Device in Normal Mode"
         elif "1281" in deviceid:
             devicestatus = "Device in Recovery Mode"
         else:
             devicestatus =  "No device found!"
+    return devicestatus
 
